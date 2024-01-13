f86b00e6f4f4f94c82635f44484072b095ab6455
Guang-an <156215249+Guang-an@users.noreply.github.com>
2024-01-13 17:52:09 +0800

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,27 @@
+class Course {
+    String title;
+    String teacher;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String toString() {
+        return "Course{" +
+                "title='" + title + '\'' +
+                ", teacher='" + teacher + '\'' +
+                '}';
+    }
+}
